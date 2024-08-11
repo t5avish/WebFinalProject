53c50e05cb6c698031805f2898a550e24c2a70c4
OmerFatal <omer.omer92@gmail.com>
2024-08-11 14:31:05 +0300

    move the "Add new challange" to the top of the screen "Challanges"

--- client/src/components/ChallengesPage.js
+++ client/src/components/ChallengesPage.js
@@ -105,6 +105,12 @@ const ChallengesPage = () => {
   return (
     <section id="challenges" className="container mx-auto mt-8 p-4">
       <h2 className="text-3xl font-bold text-gray-800 dark:text-white text-center mb-8">Fitness Challenges</h2>
+
+      <div className="text-center mb-12">
+        <h3 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Want to add a new challenge?</h3>
+        <button onClick={handleAddChallengeClick} className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-900 dark:bg-gray-600 dark:hover:bg-gray-500">Add New Challenge</button>
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {challenges.map(challenge => (
           <div key={challenge._id} className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
@@ -118,10 +124,6 @@ const ChallengesPage = () => {
             </button>
           </div>
         ))}
-      </div>
-      <div className="text-center mt-12">
-        <h3 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Want to add a new challenge?</h3>
-        <button onClick={handleAddChallengeClick} className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-900 dark:bg-gray-600 dark:hover:bg-gray-500">Add New Challenge</button>
       </div>
 
       {showForm && (
