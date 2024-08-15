da40480f82d63d4e8389f8e14a553c2ce6d2a346
OmerFatal <omer.omer92@gmail.com>
2024-08-15 18:16:14 +0300

    Changed Challenges to Custom Hooks

--- client/src/components/Challenges/ChallengesHeader.js
+++ client/src/components/Challenges/ChallengesHeader.js
@@ -4,7 +4,6 @@ const ChallengesHeader = ({ handleAddChallengeClick }) => {
   return (
     <section className="container mx-auto mt-8 p-4">
       <h2 className="text-3xl font-bold text-gray-800 dark:text-white text-center mb-8">Fitness Challenges</h2>
-
       <div className="text-center mb-12">
         <h3 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Want to add a new challenge?</h3>
         <button
